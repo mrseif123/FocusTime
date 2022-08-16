9946f19a6f0481029943a59cab9fee28314f077e
Seaf Aliyan <codo47@gmail.com>
2022-08-16 18:31:10 +0300

    initial screen with simple input:

---
+++ App.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import Constants from 'expo-constants';
+import { Focus } from './src/features/Focus';
+
+import { colors } from './src/utils/colors';
+
+export default function App() {
+  const [currentSubject, setCurrentSubject] = React.useState(null);
+  return (
+    <SafeAreaView style={styles.container}>
+      {!currentSubject ? (
+        <Focus addSubject={setCurrentSubject} />
+      ) : (
+        <Text style={{color: colors.white}}>Here will be a timer for {currentSubject}</Text>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+});
